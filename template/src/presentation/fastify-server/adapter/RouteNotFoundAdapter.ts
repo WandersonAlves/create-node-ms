3fe0632efa4064aaaf39370e4538788fdade1ad1
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2020-06-29 22:36:57 -0300

    fix(fastify): add missing 404 adapter

---
+++ template/src/presentation/fastify-server/adapter/RouteNotFoundAdapter.ts
@@ -0,0 +1,13 @@
+import { FastifyRequest, FastifyReply } from 'fastify';
+import { NOT_FOUND } from 'http-status';
+import RouteNotFoundMiddleware from '../../../shared/server/RouteNotFoundMiddleware';
+
+export default class ExpressRouteNotFoundAdapter {
+  static adapt() {
+    return (request: FastifyRequest, reply: FastifyReply<any>) => {
+      const originalUrl = request.originalUrl;
+      const method = request.method;
+      return reply.status(NOT_FOUND).send(RouteNotFoundMiddleware(originalUrl, method));
+    };
+  }
+}
