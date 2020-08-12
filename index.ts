8fc92de7c4eb25fdacef1dccdcb91de64bf7b1e2
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2020-08-11 21:14:55 -0300

    feat: add projectPath option

--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ program
   .description("Generate a CLEAN nodejs project with TypeScript")
   .option("--noCommit", "Don't do a first commit")
   .option("--useNpm", "Use npm to install dependencies")
+  .option("-p, --path <projectPath>", "Generated project path")
   .requiredOption("-pn, --projectName <projectName>", "Project name")
   .action(CreateTemplateCmd);
 
