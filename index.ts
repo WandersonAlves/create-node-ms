0b15a5b7e3f4302ab4b3ae62780a56e81110d9b3
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2020-08-27 02:53:30 -0300

    fix(core): handle --projectPath param

--- index.ts
+++ index.ts
@@ -5,11 +5,11 @@ import { CreateNodeMsCmd } from './scripts/create-node-ms';
 program
   .version('0.0.1', '-v, --version')
   .description('Generate a CLEAN nodejs project with TypeScript')
-  .option('--noCommit', "Don't do a first commit")
-  .option('--useNpm', 'Use npm to install dependencies')
-  .option('-p, --path <projectPath>', 'Generated project path')
   .requiredOption('-pn, --projectName <projectName>', 'Project name')
   .requiredOption('-e, --entityName <entityName>', 'Name of the first entity')
+  .option('--noCommit', "Don't do a first commit")
+  .option('--useNpm', 'Use npm to install dependencies')
+  .option('-p, --projectPath <projectPath>', 'Generated project path')
   .option('-ep, --entityPluralName <entityPluralName>', 'Pluralized name of the entity')
   .option('--verbose', 'Output verbose info')
   .action(CreateNodeMsCmd);
--- scripts/create-node-ms.ts
+++ scripts/create-node-ms.ts
@@ -1,5 +1,6 @@
 import * as path from 'path';
 import * as cp from 'child_process';
+import * as appRoot from 'app-root-path';
 import { logger, jsonString } from '../utils/logger';
 import { capitalizeString } from '../utils/miscUtils';
 import { processTemplate } from '../utils/templateProcessing';
@@ -41,19 +42,19 @@ export const CreateNodeMsCmd = async ({
     })}`,
   );
 
-  const rootDir = path.join(__filename);
+  const rootDir = path.join(appRoot.path);
   logger.verbose(`RootDir: ${rootDir}`);
   const currentDir = cp.execSync('pwd').toString().trim();
   logger.verbose(`CurrentDir: ${currentDir}`);
-  const serviceDir = `${currentDir}/${projectName}`;
+  const serviceDir = path.join(path.join(currentDir, projectPath || ''), projectName);
   logger.verbose(`ServiceDir: ${serviceDir}`);
-  const cpTemplatePath = path.join(rootDir, '..', '..', '..', TEMPLATE_FOLDER);
+  const cpTemplatePath = path.join(rootDir, TEMPLATE_FOLDER);
   logger.verbose(`CPTemplateDir: ${cpTemplatePath}`);
   const cpTemplatePackageJson = path.join(cpTemplatePath, '.package.json');
   logger.verbose(`CPTemplatePackageJson: ${cpTemplatePackageJson}`);
 
   logger.info('Creating service folder...');
-  cp.execSync(`mkdir ${projectName}`);
+  cp.execSync(`mkdir ${serviceDir}`);
   logger.info('Copying files...');
   cp.execSync(`cp -r ${cpTemplatePath}. ${serviceDir}/`);
   cp.execSync(`cp -r ${cpTemplatePackageJson} ${serviceDir}/package.json`);
