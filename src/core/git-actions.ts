83c64aae67ee5f4d5dc0aa3a372027a7ae9c57bc
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2020-08-29 22:47:03 -0300

    chore(core): fix logger menssage

--- src/core/git-actions.ts
+++ src/core/git-actions.ts
@@ -2,14 +2,14 @@ import { logger } from '../utils/logger';
 import { execSync } from 'child_process';
 
 export const gitInit = (serviceDir: string) => {
-  logger.info('Setup git...');
+  logger.info('Starting git...');
   execSync(`cd ${serviceDir} && git init`, {
     stdio: 'inherit',
   });
 };
 
 export const gitFirstCommit = (serviceDir: string) => {
-  logger.info('Setup git...');
+  logger.info('Doing first commit, yay...');
   execSync(`cd ${serviceDir} && git add . && git commit -m "feat: first commit :rocket:"`, {
     stdio: 'inherit',
   });
