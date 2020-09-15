b39108ed147f0b04297b0f4a499472068e503cc7
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2020-09-15 02:49:07 -0300

    fix(template-node): typo in repository

--- template-node-ms/src/repositories/EntityInMemoryRepository.ts
+++ template-node-ms/src/repositories/EntityInMemoryRepository.ts
@@ -5,30 +5,30 @@ import { TRestParameters } from '../shared/types';
 
 @injectable()
 export default class D_Entity_DInMemoryRepository implements DataRepository<ID_Entity_DDTO> {
-  private D_entity_DPlural: ID_Entity_DDTO[] = [];
+  private D_entities_D: ID_Entity_DDTO[] = [];
 
   find(filter?: TRestParameters<ID_Entity_DDTO>): Promise<ID_Entity_DDTO[]> {
-    const result = this.D_entity_DPlural.filter(u => this._match(u, filter));
+    const result = this.D_entities_D.filter(u => this._match(u, filter));
     return Promise.resolve(result);
   }
 
   findOne(filter?: TRestParameters<ID_Entity_DDTO>): Promise<ID_Entity_DDTO> {
-    const result = this.D_entity_DPlural.find(u => this._match(u, filter));
+    const result = this.D_entities_D.find(u => this._match(u, filter));
     return Promise.resolve(result);
   }
 
   create(obj: ID_Entity_DDTO): Promise<ID_Entity_DDTO> {
-    this.D_entity_DPlural.push(obj);
+    this.D_entities_D.push(obj);
     return Promise.resolve(obj);
   }
 
   updateById(id: string | number, obj: Partial<ID_Entity_DDTO>): Promise<ID_Entity_DDTO> {
-    this.D_entity_DPlural[id] = { ...this.D_entity_DPlural[id], ...obj };
-    return Promise.resolve(this.D_entity_DPlural[id]);
+    this.D_entities_D[id] = { ...this.D_entities_D[id], ...obj };
+    return Promise.resolve(this.D_entities_D[id]);
   }
 
   removeById(id: string | number): Promise<null> {
-    this.D_entity_DPlural = this.D_entity_DPlural.filter(e => e.id !== id);
+    this.D_entities_D = this.D_entities_D.filter(e => e.id !== id);
     return Promise.resolve(null);
   }
 
