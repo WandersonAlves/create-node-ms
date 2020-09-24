2380736e91638c0d28183df898e6e1a4f5ae66bf
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2020-09-23 23:20:05 -0300

    fix(template-node): UnprocessableObjectException name collision

--- template-node-ms/src/shared/exceptions/UnprocessableObjectException.ts
+++ template-node-ms/src/shared/exceptions/UnprocessableObjectException.ts
@@ -1,16 +1,16 @@
 import * as httpCodes from 'http-status';
 import GenericException from './GenericException';
 
-export default class UnprocessableD_Entity_DException extends GenericException {
+export default class UnprocessableObjectException extends GenericException {
   constructor(errors: any) {
     const params = {
-      name: 'UnprocessableD_Entity_DException',
+      name: 'UnprocessableObjectException',
       message: 'Fields validation failed',
       extras: errors,
       statusCode: httpCodes.UNPROCESSABLE_ENTITY,
     };
     super(params);
 
-    Object.setPrototypeOf(this, UnprocessableD_Entity_DException.prototype);
+    Object.setPrototypeOf(this, UnprocessableObjectException.prototype);
   }
 }
