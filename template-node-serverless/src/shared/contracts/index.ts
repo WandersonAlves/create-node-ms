d2b3e66c298a0910ee29f53de89e4a4d968489e6
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2020-10-13 12:02:37 -0300

    fix(node-serverless): use interface instead abstract class

--- template-node-serverless/src/shared/contracts/index.ts
+++ template-node-serverless/src/shared/contracts/index.ts
@@ -9,19 +9,18 @@ export interface UseCase {
   execute(any?: any): Promise<IHttpResponse>;
 }
 
-export abstract class DataRepository<T = any> {
-  abstract find(filter?: TRestParameters<T>): Promise<T[]>;
-  abstract findOne(filter?: TRestParameters<T>): Promise<T>;
+export interface DataRepository<T = any> {
+  find(filter?: TRestParameters<T>): Promise<T[]>;
+  findOne(filter?: TRestParameters<T>): Promise<T>;
 
-  abstract create(obj: T): Promise<T>;
-  abstract updateById(id: string | number, obj: Partial<T>): Promise<T>;
-  abstract removeById(id: string | number): Promise<null>;
+  create(obj: T): Promise<T>;
+  updateById(id: string | number, obj: Partial<T>): Promise<T>;
+  removeById(id: string | number): Promise<null>;
 }
 
-// tslint:disable-next-line: max-classes-per-file
-export abstract class DatabaseConnection {
-  abstract async connect(): Promise<this>;
-  abstract async disconnect(): Promise<void>;
+export interface DatabaseConnection {
+  connect(): Promise<this>;
+  disconnect(): Promise<void>;
 }
 
 export interface IHttpRequest<BODY = any, QUERY = any, PARAMS = any> {
@@ -43,7 +42,6 @@ export interface IHttpError {
   details?: any;
 }
 
-// tslint:disable-next-line: max-classes-per-file
-export abstract class RequestRouter {
-  abstract route(req: IHttpRequest): Promise<IHttpResponse>;
+export interface RequestRouter {
+  route(req: IHttpRequest): Promise<IHttpResponse>;
 }
