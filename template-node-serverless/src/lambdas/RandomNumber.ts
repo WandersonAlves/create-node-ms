0bcfa87d483541d47adb0865dcff92affbc20576
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2020-10-29 12:51:44 -0300

    feat(node-serverless): add @types/aws-lambda

--- template-node-serverless/src/lambdas/RandomNumber.ts
+++ template-node-serverless/src/lambdas/RandomNumber.ts
@@ -1,11 +1,12 @@
 import 'reflect-metadata';
+import { APIGatewayProxyResult } from 'aws-lambda';
 import { ServerlessFactory } from '../shared/factory/ServerlessFactory';
 import container from '../infra/container/inversify.config';
 import GenerateRandomNumberCase from '../cases/GenerateRandomNumberCase';
 
 const useCase = container.get<GenerateRandomNumberCase>(GenerateRandomNumberCase);
 
-export const handle = async () => {
+export const handle = async (): Promise<APIGatewayProxyResult> => {
   const result = await useCase.execute();
   return ServerlessFactory.LambdaResponse(result);
 };
--- template-node-serverless/src/shared/factory/ServerlessFactory.ts
+++ template-node-serverless/src/shared/factory/ServerlessFactory.ts
@@ -1,7 +1,8 @@
+import { APIGatewayProxyResult } from 'aws-lambda';
 import { IHttpResponse } from '../../shared/contracts';
 
 export class ServerlessFactory {
-  static LambdaResponse(response: IHttpResponse<any>) {
+  static LambdaResponse(response: IHttpResponse<any>): APIGatewayProxyResult {
     return {
       ...response,
       body: JSON.stringify(response?.body),
