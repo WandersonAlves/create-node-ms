dcd7bd8ee03e58be7db86e77e537f1c3a8e9685e
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2020-11-06 18:27:49 -0300

    fix(nse,nsl): wrong interface paths

---
+++ templates/template-node-serverless-lambda/tests/cases/GenerateRandomNumberCase.spec.ts
@@ -0,0 +1,31 @@
+import 'reflect-metadata';
+import { expect } from 'chai';
+import GenerateRandomNumberCase from '../../src/cases/GenerateRandomNumberCase';
+import HttpResponse from '../../src/shared/responses/HttpResponse';
+import container from '../../src/infra/container/inversify.config';
+
+const _bindHublogRepoMock = () => null;
+
+const _getUseCase = () => {
+  return container.get<GenerateRandomNumberCase>(GenerateRandomNumberCase);
+};
+
+describe('GenerateRandomNumberCase', () => {
+  beforeEach(() => {
+    // create a snapshot so each unit test can modify
+    // it without breaking other unit tests
+    container.snapshot();
+  });
+
+  afterEach(() => {
+    // Restore to last snapshot so each unit test
+    // takes a clean copy of the application container
+    container.restore();
+  });
+
+  it('Should get a random number between 1 and 10', async () => {
+    _bindHublogRepoMock();
+    const result = (await _getUseCase().execute()) as HttpResponse<{ number: number }>;
+    expect(result.body.number).to.be.within(1, 10);
+  });
+});
