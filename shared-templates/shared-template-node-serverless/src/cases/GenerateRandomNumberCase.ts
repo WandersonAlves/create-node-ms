a03f980aee501db391b1cedbd060e7469c24f002
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2021-06-09 22:01:35 -0300

    feat(nsl): update generator

--- shared-templates/shared-template-node-serverless/src/cases/GenerateRandomNumberCase.ts
+++ shared-templates/shared-template-node-serverless/src/cases/GenerateRandomNumberCase.ts
@@ -1,14 +1,14 @@
-import { IHttpError, UseCase } from '../shared/contracts';
 import { OK } from 'http-status';
+import { UseCase } from '../shared/contracts';
 import { injectable } from 'inversify';
 import ExceptionHandler from '../shared/decorators/ExceptionHandler';
 import HttpResponse from '../shared/responses/HttpResponse';
 
 @injectable()
-export default class GenerateRandomNumberCase implements UseCase<{ number: number }, IHttpError> {
-  @ExceptionHandler()
+export default class GenerateRandomNumberCase implements UseCase {
+  @ExceptionHandler
   async execute() {
     const number = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
-    return HttpResponse.success(OK, { number });
+    return HttpResponse.success({ status: OK, body: { number } });
   }
 }
