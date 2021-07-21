3c47b4b4528b13f9db931b89132d86a6863d9ff0
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2021-07-21 14:26:05 -0300

    chore(nse,nsl): remove unused types

--- shared-templates/shared-template-node-serverless/src/shared/contracts/index.ts
+++ shared-templates/shared-template-node-serverless/src/shared/contracts/index.ts
@@ -1,36 +1,18 @@
-import { TRestParameters, UnkownParams } from '../types';
+import { UnkownParams } from '../types';
 import HttpResponse from '../responses/HttpResponse';
 
-export interface IPaginationRequestParams {
-  skip?: number;
-  limit?: number;
-}
-
-export interface UseCaseParams<H = any, B = any, Q = any> {
+export interface UseCaseParams<H = any, B = any, Q = any, P = any> {
   headers?: H;
   body?: B;
   query?: Q;
+  pathParams?: P;
 }
 
 export interface UseCase {
   execute({ headers, body }: UseCaseParams): Promise<HttpResponse<any>>;
 }
 
-export interface DataRepository<T = any> {
-  find(filter?: TRestParameters<T>): Promise<T[]>;
-  findOne(filter?: TRestParameters<T>): Promise<T>;
-
-  create(obj: T): Promise<T>;
-  updateById(id: string | number, obj: Partial<T>): Promise<T>;
-  removeById(id: string | number): Promise<null>;
-}
-
-export interface DatabaseConnection {
-  connect(): Promise<this>;
-  disconnect(): Promise<void>;
-}
-
-export interface IHttpRequest<BODY = any, QUERY = any, PARAMS = any, HEADERS = any> {
+export interface IHttpRequest<HEADERS = any, QUERY = any, PARAMS = any, BODY = any> {
   body?: BODY;
   query?: QUERY;
   params?: PARAMS;
--- shared-templates/shared-template-node-serverless/src/shared/types/index.ts
+++ shared-templates/shared-template-node-serverless/src/shared/types/index.ts
@@ -1,9 +1,4 @@
-import { IPaginationRequestParams, RequestRouter } from '../contracts';
+import { RequestRouter } from '../contracts';
 
-export type TGenericPropertiesToBoolean<T> = { [P in keyof T]?: boolean };
-export type TGenericPropertiesToSort<T> = { [P in keyof T]?: 1 | -1 };
-export type TGenericPropertiesToString<T> = keyof T;
-export type TRestParameters<T> = Partial<T> & IPaginationRequestParams;
-export type HeaderConvert = { original: string; updated: string }[];
 export type Newable<T = RequestRouter> = new (...args: any[]) => T;
 export type UnkownParams<T = any> = { [k: string]: T };
