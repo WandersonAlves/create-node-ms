ee796c51fdd66a39947ecb98b236e280ab2c64ea
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2021-12-14 12:38:56 -0300

    fix: internal logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,12 +1,10 @@
 import { createLogger, format, transports } from 'winston';
+import { join } from 'path';
 
 const { printf, colorize } = format;
 const colorizer = colorize();
 
-const timestampFormatter = () =>
-  new Date().toLocaleString('pt-BR', {
-    timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
-  });
+const timestampFormatter = () => new Date().toISOString();
 
 const consoleFormatter = printf(({ stack, level, message, label, timestamp }: { [key: string]: string }) => {
   const levelLabelColorized = colorizer.colorize(level, `${level}:${label}`);
@@ -18,15 +16,26 @@ const baseLogger = createLogger({
   level: 'info',
   defaultMeta: { label: 'main' },
   format: format.combine(format.errors({ stack: true }), format.timestamp({ format: timestampFormatter })),
-  transports: new transports.Console({
-    format: consoleFormatter,
-  }),
 });
 
 /**
- * Returns a pretty representation of a json for a give object
+ * Returns a pretty represetation of a json for a give object
+ *
  * @param obj An object
  */
 export const jsonString = (obj: any) => `\n${JSON.stringify(obj, null, 2)}\n`;
+
+baseLogger
+  .add(
+    new transports.Console({
+      format: consoleFormatter,
+    }),
+  )
+  .add(
+    new transports.File({
+      format: consoleFormatter,
+      filename: join(__dirname, '..', '..', 'log.log'),
+    }),
+  );
 
 export const logger = baseLogger;
