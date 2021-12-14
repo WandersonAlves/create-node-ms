361139885a90de30bc43cf0806e3c370463df163
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2021-12-14 15:16:49 -0300

    feat(nse): RoutesBuilder accepts middleware

--- templates/template-node-serverless-express/src/utils/RoutesBuilder.ts
+++ templates/template-node-serverless-express/src/utils/RoutesBuilder.ts
@@ -1,32 +1,31 @@
 import { GetFromContainer } from '@infra/container/inversify.config';
 import { Newable } from '@shared/types';
+import { RequestHandler } from 'express-serve-static-core';
 import { Router } from 'express';
 import ExpressRouterAdapter from '@shared/adapters/ExpressRouterAdapter';
-
 /**
  * Creates a express route using the build pattern
  */
 export default class RoutesBuilder {
   private _router = Router();
-
-  get(url: string | string[], requestRouter: Newable) {
-    this._router.get(url, ExpressRouterAdapter.adapt(GetFromContainer(requestRouter)));
+  get(url: string | string[], requestRouter: Newable, ...middleware: RequestHandler[]) {
+    this._router.get(url, middleware, ExpressRouterAdapter.adapt(GetFromContainer(requestRouter)));
     return this;
   }
-  post(url: string | string[], requestRouter: Newable) {
-    this._router.post(url, ExpressRouterAdapter.adapt(GetFromContainer(requestRouter)));
+  post(url: string | string[], requestRouter: Newable, ...middleware: RequestHandler[]) {
+    this._router.post(url, middleware, ExpressRouterAdapter.adapt(GetFromContainer(requestRouter)));
     return this;
   }
-  put(url: string | string[], requestRouter: Newable) {
-    this._router.put(url, ExpressRouterAdapter.adapt(GetFromContainer(requestRouter)));
+  put(url: string | string[], requestRouter: Newable, ...middleware: RequestHandler[]) {
+    this._router.put(url, middleware, ExpressRouterAdapter.adapt(GetFromContainer(requestRouter)));
     return this;
   }
-  delete(url: string | string[], requestRouter: Newable) {
-    this._router.delete(url, ExpressRouterAdapter.adapt(GetFromContainer(requestRouter)));
+  delete(url: string | string[], requestRouter: Newable, ...middleware: RequestHandler[]) {
+    this._router.delete(url, middleware, ExpressRouterAdapter.adapt(GetFromContainer(requestRouter)));
     return this;
   }
-  patch(url: string | string[], requestRouter: Newable) {
-    this._router.patch(url, ExpressRouterAdapter.adapt(GetFromContainer(requestRouter)));
+  patch(url: string | string[], requestRouter: Newable, ...middleware: RequestHandler[]) {
+    this._router.patch(url, middleware, ExpressRouterAdapter.adapt(GetFromContainer(requestRouter)));
     return this;
   }
   build() {
