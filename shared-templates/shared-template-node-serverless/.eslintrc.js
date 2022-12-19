89f244f15b10d34752aba133bdab4429936530ce
WandersonAlves <wandersonalvesferreira2012@gmail.coms>
2022-12-18 23:37:06 -0300

    feat: update eslint

--- shared-templates/shared-template-node-serverless/.eslintrc.js
+++ shared-templates/shared-template-node-serverless/.eslintrc.js
@@ -29,7 +29,6 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'prettier',
-    'prettier/@typescript-eslint',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
